53681cc59a7bf464da6551fed74d4ca4e5ab3a8e
Yahor Maksimchyk <yahor.maksimchyk@gmail.com>
2025-03-12 13:09:01 +0300

    1-2, economy + mock code working

--- application/app/src/main/java/com/example/iot/data/local/device/state/DeviceStateDAO.kt
+++ application/app/src/main/java/com/example/iot/data/local/device/state/DeviceStateDAO.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DeviceStateDAO {
@@ -11,10 +12,16 @@ interface DeviceStateDAO {
     @Query("SELECT * FROM device_state WHERE deviceId = :deviceId LIMIT 1")
     suspend fun getDeviceStateByDeviceId(deviceId: Int): DeviceState?
 
+    @Query("SELECT * FROM device_state WHERE deviceId = :deviceId")
+    fun getDevicesStateByDeviceId(deviceId: Int): Flow<DeviceState?>
+
     @Insert
     suspend fun insertDeviceState(deviceState: DeviceState)
 
     @Update
     suspend fun updateDeviceState(deviceState: DeviceState)
+
+    @Query("SELECT state FROM device_state WHERE deviceId = :deviceId")
+    suspend fun getDeviceState(deviceId: Int): String?
 }
 
