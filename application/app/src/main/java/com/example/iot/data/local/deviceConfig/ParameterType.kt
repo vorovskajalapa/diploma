8d069ccab6f947b33c57957b3ffe3dbe475d543d
Yahor <yahor.maksimchyk@gmail.com>
2025-04-30 11:05:47 +0300

    rm old proj, add docs

--- application/app/src/main/java/com/example/iot/data/local/deviceConfig/ParameterType.kt
+++
@@ -1,7 +0,0 @@
-package com.example.iot.data.local.deviceConfig
-
-enum class ParameterType {
-    READONLY,   // for important params
-    SWITCH,     // on/off
-    SELECT      // value todo: change to slider
-}
--- application/app/src/main/java/com/example/iot/ui/viewmodel/HomeViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.iot.ui.viewmodel
-
-import androidx.lifecycle.ViewModel
-import com.example.iot.data.local.AppDatabase
-
-
-class HomeViewModel(db: AppDatabase) : ViewModel()
